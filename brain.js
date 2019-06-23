8d2773433d852acc27fc6763429db729a6749cd3
marko-polo-cheno <37851975+marko-polo-cheno@users.noreply.github.com>
2019-06-23 16:35:32 -0400

    Added Files to Github
    Yay

---
+++ brain.js
@@ -0,0 +1,61 @@
+let toDoList = [
+  {'habit':'Brush', 'done':false},
+  {'habit':'Pray', 'done':false},
+  {'habit':'Bible', 'done':false},
+  {'habit':'Eat', 'done':false},
+  {'habit':'Exercise', 'done':false},
+  {'habit':'Study', 'done':false},
+  {'habit':'Sleep', 'done':false}
+];
+let green = 'rgb(50, 205, 50)';
+let white = "#eee";
+
+chrome.storage.local.get('currentDate', function getData(data) {
+  let currentDate = data.currentDate;
+  let settingButton = document.getElementById('settingButton');
+  settingButton.innerHTML = currentDate;
+});
+
+chrome.storage.local.get('toDoList', function getData(data) {
+  toDoList = data.toDoList;
+  for (let x = 0; x < 7; x++) {
+    let button = document.createElement("button");
+    button.innerHTML = toDoList[x].habit;
+    button.id = x;
+  
+    if (toDoList[x].done) {
+      button.style.background = green;
+    }
+    button.addEventListener("click", function useSettings() {
+      if (document.getElementById(x).style.backgroundColor == green) {
+        document.getElementById(x).style.background = white;
+        toDoList[x].done = false;
+      } else {
+        document.getElementById(x).style.background = green;
+        toDoList[x].done = true;
+      }
+      chrome.storage.local.set({'toDoList': toDoList});
+    });
+    
+    var addButtonsHere = document.getElementById("add-buttons-here");
+    addButtonsHere.appendChild(button);
+  }
+});
+
+chrome.storage.local.set(
+  {
+    'currentDate': getDate(),
+    'toDoList' : toDoList
+  }
+);
+
+
+function getDate() {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let yyyy = today.getFullYear();
+
+  today = mm + '/' + dd + '/' + yyyy;
+  return today;
+}
